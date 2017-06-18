5c3678bd853c51692712f62061c55f960ba2d545
uupaa <uupaa.js@gmail.com>
2017-06-18 22:28:31 +0900

    update

--- index.js
+++ index.js
@@ -1,70 +1,64 @@
 
 module.exports = {
-  installModules: webapp2tools_installModules,
+  module: {
+    copy: module_copy,
+  }
 };
 
-const installModulesOptions = {
-  branch: "master",
-  jsonKeyword: "es6",
-  jsonFileName: "package.json",
-};
-
-function webapp2tools_installModules(es6ModulesList, // @arg ArrayArray - [ [ sourcePath, targetPath ], [ sourcePath, targetPath ], ... ]
-                                     options = {}) { // @arg Object = {} - { type, branch, jsonKeyword, jsonFileName }
+function module_copy(list,           // @arg ModulesListArrayObject - [ { from: sourcePath, to: targetPath }, ... ]
+                     options = {}) { // @arg Object = {} - { json, property, branch }
   // Copy sourcePath to targetPath.
-  // If sourcePath is json, get information of es6.js to be copied from json[jsonKeyword] and use it as sourcePath.
+  // If sourcePath is json, get information of es6.js to be copied from json[property] and use it as sourcePath.
   //
-  //  const TARGET_DIR = "./app/assets/modules";
-  //  const es6ModulesList = [
-  //    // sourcePath                                                  targetPath
-  //    [ `https://github.com/uupaa/foo`,                              `${TARGET_DIR}/foo.js` ], // remote
-  //    [ `https://github.com/uupaa/foo#es6`,                          `${TARGET_DIR}/foo.js` ], // remote
-  //    [ `https://github.com/uupaa/foo/blob/master/app/bundle.es6.js`,`${TARGET_DIR}/foo.js` ], // remote
-  //    [ `https://github.com/uupaa/foo/blob/master/package.json#es6`, `${TARGET_DIR}/foo.js` ], // remote
-  //    [ `./node_modules/foo`,                                        `${TARGET_DIR}/foo.js` ], // local
-  //    [ `./node_modules/foo#es6`,                                    `${TARGET_DIR}/foo.js` ], // local
-  //    [ `./node_modules/foo/app/bundle.es6.js`,                      `${TARGET_DIR}/foo.js` ], // local
-  //    [ `./node_modules/foo/package.json#es6`,                       `${TARGET_DIR}/foo.js` ], // local
+  //  const dir = "./app/assets/modules";
+  //  const list = [
+  //    { from: `https://github.com/uupaa/foo`,                                 to: `${dir}/foo.js` }, // remote install
+  //    { from: `https://github.com/uupaa/foo#es6js`,                           to: `${dir}/foo.js` }, // remote
+  //    { from: `https://github.com/uupaa/foo/blob/master/app/bundle.es6.js`,   to: `${dir}/foo.js` }, // remote
+  //    { from: `https://github.com/uupaa/foo/blob/master/package.json#es6js`,  to: `${dir}/foo.js` }, // remote
+  //    { from: `./node_modules/foo`,                                           to: `${dir}/foo.js` }, // local install
+  //    { from: `./node_modules/foo#es6js`,                                     to: `${dir}/foo.js` }, // local
+  //    { from: `./node_modules/foo/app/bundle.es6.js`,                         to: `${dir}/foo.js` }, // local
+  //    { from: `./node_modules/foo/package.json#es6js`,                        to: `${dir}/foo.js` }, // local
   //  ];
   //
-  // let tool = require("webapp2-node-tools");
-  // tool.installModules(MODULE_LIST, options);
+  // require("webapp2-node-tools").module.copy(list, options);
 
-  options.branch       = options.branch       || installModulesOptions.branch;
-  options.jsonKeyword  = options.jsonKeyword  || installModulesOptions.jsonKeyword;
-  options.jsonFileName = options.jsonFileName || installModulesOptions.jsonFileName;
+  options.json     = options.json     || "package.json";
+  options.property = options.property || "es6js";
+  options.branch   = options.branch   || "master";
 
   const urlParser = require("url");
 
-  es6ModulesList.forEach(pathPair => {
-    let [ sourcePath, targetPath ] = pathPair;
+  list.forEach(fromTo => {
+    let { from, to } = fromTo;
 
-    if (!sourcePath.length) {
-      console.error(`Error: [sourcePath = ${sourcePath}, targetPath = ${targetPath}], sourcePath is empty`);
+    if (!from || from.length) {
+      console.error(`Error: { from: ${from}, to: ${to} }, from is empty`);
       return;
     }
-    if (!targetPath.length) {
-      console.error(`Error: [sourcePath = ${sourcePath}, targetPath = ${targetPath}], targetPath is empty`);
+    if (!to || to.length) {
+      console.error(`Error: { from: ${from}, to: ${to} }, to is empty`);
       return;
     }
 
-    let { host, pathname, hash } = urlParser.parse(sourcePath);
+    let { host, pathname, hash } = urlParser.parse(from);
     // https://github.com/uupaa/foo  /master/package.json #es6
     //         ~~~~~~~~~~ ~~~~~ ~~~  ~~~~~~~~~~~~~~~~~~~~ ~~~~
     //         host       owner repo path                 hash
-    const jsonKeyword = hash ? hash.slice(1) : options.jsonKeyword; // "#es6".slice(1) -> "es6"
+    const property = hash ? hash.slice(1) : options.property; // "#es6".slice(1) -> "es6"
 
     if (host) {
-      let { configURL, es6jsURL } = _resolveRemoteURL(host, pathname, hash, targetPath, options);
-      _copyToLocal(sourcePath, targetPath, configURL, es6jsURL, jsonKeyword, true);
+      let { jsonURL, es6jsURL } = _resolveRemoteURL(host, pathname, hash, to, options);
+      _copyToLocal(from, to, jsonURL, es6jsURL, property, true);
     } else {
-      let { configURL, es6jsURL } = _resolveLocalPath("", pathname, hash, targetPath, options);
-      _copyToLocal(sourcePath, targetPath, configURL, es6jsURL, jsonKeyword, false);
+      let { jsonURL, es6jsURL } = _resolveLocalPath("", pathname, hash, to, options);
+      _copyToLocal(from, to, jsonURL, es6jsURL, property, false);
     }
   });
 }
 
-function _resolveRemoteURL(host, pathname, hash, targetPath, options) {
+function _resolveRemoteURL(host, pathname, hash, to, options) {
   // https://raw.githubusercontent.com/uupaa/foo.js /master/package.json #es6
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~ ~~~~~~ ~~~~~~~~~~~~~~~~~~~~ ~~~~
   //          baseURL                  owner repo   path                 hash
@@ -73,7 +67,7 @@ function _resolveRemoteURL(host, pathname, hash, targetPath, options) {
   const repo = path[1];
   const lastPath = path[path.length - 1];
   let baseURL = "";
-  let configURL = "";
+  let jsonURL = "";
   let es6jsURL = "";
 
   if (host === "github.com") {
@@ -89,64 +83,64 @@ function _resolveRemoteURL(host, pathname, hash, targetPath, options) {
     // https://github.com/${owner}/${repo}/blob/${branch}/*.json
     //                                                    ~~~~~~~~
     //                                                    lastPath
-    configURL = [baseURL, owner, repo, options.branch, lastPath].join("/");
-    es6jsURL = [baseURL, owner, repo, options.branch].join("/");
+    jsonURL = [baseURL, owner, repo, options.branch, lastPath].join("/");
+    es6jsURL = [baseURL, owner, repo, options.branch].join("/") + "/"; // + json[property]
   } else {
     // https://github.com/${owner}/${repo}
-    configURL = [baseURL, owner, repo, options.branch, options.jsonFileName].join("/");
-    es6jsURL = [baseURL, owner, repo, options.branch].join("/");
+    jsonURL = [baseURL, owner, repo, options.branch, options.json].join("/");
+    es6jsURL = [baseURL, owner, repo, options.branch].join("/") + "/"; // + json[property]
   }
-  return { configURL, es6jsURL };
+  return { jsonURL, es6jsURL };
 }
 
-function _resolveLocalPath(host, pathname, hash, targetPath, options) {
+function _resolveLocalPath(host, pathname, hash, to, options) {
   // ./node_modules/Spec.js/package.json #es6
   // ~~~~~~~~~~~~~~ ~~~~~~~ ~~~~~~~~~~~~ ~~~~
   // dir            mod     path         hash
   const path = pathname.slice(0).split("/");
   const lastPath = path[path.length - 1];
-  let configURL = "";
+  let jsonURL = "";
   let es6jsURL = "";
 
   if (/\.es6\.js$/.test(lastPath)) { // last path is bundled js (eg: "foo.es6.js")
     es6jsURL = pathname;
   } else if (/\.json$/.test(lastPath)) { // last path is json (maybe package.json)
-    configURL = pathname;
+    jsonURL = pathname;
   } else {
-    configURL = pathname + options.jsonFileName;
+    jsonURL = pathname + "/" + options.json;
   }
-  return { configURL, es6jsURL };
+  return { jsonURL, es6jsURL };
 }
 
-function _copyToLocal(sourcePath, targetPath, configURL, es6jsURL, jsonKeyword, remote) {
+function _copyToLocal(from, to, jsonURL, es6jsURL, property, remote) {
   const fs = require("fs");
 
-  if (configURL) {
-    _fetch(configURL, "json", json => {
-      if (!(jsonKeyword in json)) {
+  if (jsonURL) {
+    _fetch(jsonURL, "json", json => {
+      if (!(property in json)) {
         _jsonKeywordNotFound();
       } else {
-        _fetch(es6jsURL + "/" + json[jsonKeyword], "string", es6js => {
-          fs.writeFile(targetPath, es6js, "utf8", () => {});
+        _fetch(es6jsURL + json[property], "string", es6js => {
+          fs.writeFile(to, es6js, "utf8", () => {});
           _success();
         }, _fetchError, 2000, remote);
       }
     }, _fetchError, 2000, remote);
   } else if (es6jsURL) {
     _fetch(es6jsURL, "string", es6js => {
-      fs.writeFile(targetPath, es6js, "utf8", () => {});
+      fs.writeFile(to, es6js, "utf8", () => {});
       _success();
     }, _fetchError, 2000, remote);
   }
 
   function _success() {
-    console.log(`Copy ${sourcePath} to ${targetPath}`);
+    console.log(`Copy ${from} to ${to}`);
   }
   function _jsonKeywordNotFound() {
-    console.error(`json keyword ${jsonKeyword} is not found in ${configURL}`);
+    console.error(`json property ${property} is not found in ${jsonURL}`);
   }
   function _fetchError(error, url, code) {
-    console.error(`Error ${sourcePath} to ${targetPath}, code=${code}`);
+    console.error(`Error ${from} to ${to}, code=${code}`);
   }
 }
 
